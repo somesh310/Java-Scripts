f069027f4058026d4a1a2a8e426a0a3fcb9eba8f
somesh310 <someshbaliarsingh310@gmail.com>
2025-10-02 17:45:36 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const AccountId=785407
+let accountEmail="someshbaliarsngh@gmail.com"
+var password="somesh@2005"
+accountCity="ranpur"
+
+// AccountId=457841
+
+console.log(AccountId);
+
+accountEmail="somedh@2333"
+password="2222"
+accountCity="nayagarh"
+
+console.table([AccountId,accountEmail,accountCity])
