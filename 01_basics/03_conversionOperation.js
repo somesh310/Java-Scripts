3c6890bc2c54299a0da43d19a62a8983159e31ee
somesh310 <someshbaliarsingh310@gmail.com>
2025-10-03 04:08:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="64" //64
+// let score="54aft" //nan
+// let score=true //1
+// let score=null //0
+// let score=undefined //nan
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let ValueInNumber=Number(score)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
+
+
+
+// let islogin=1 //true
+// let islogin=0 //false
+// let islogin="" //false
+// let islogin="somesh" //true
+
+let numberToBollean=Boolean(islogin)
+console.log(numberToBollean)
+
+
