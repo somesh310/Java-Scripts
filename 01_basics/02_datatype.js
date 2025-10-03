c91cc49f4442cf80b1f3c832114018ebc1393a2e
somesh310 <someshbaliarsingh310@gmail.com>
2025-10-03 04:06:42 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js as newer version
+
+alert(3+8) // it gives error because we use node not browser
+
+/*
+    primitive data type
+    1->number
+    2->bigint
+    3->string
+    4->boolean
+    5->null
+    6->undefined
+    7->symbol
+
+
+
+    non primitive data type
+    1->object
+*/
